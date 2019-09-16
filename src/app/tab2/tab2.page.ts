85d40c8c4764e3dbaf040a1ea6e9db4cda5a9fef
jaimecastrillon <jaimecastrillon@outlook.com>
2019-09-15 20:56:28 -0500

    Ionic countries

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CountryService } from '../api/country.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  countries: any;
+  constructor(private countryService: CountryService) { }
+
+  ngOnInit() {
+    this.countryService.getCountries().subscribe((countries: any) => {
+      this.countries = countries;
+      console.log(countries);
+    });
+  }
 
 }
